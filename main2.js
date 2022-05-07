fb7d812e8bc7a2462288a9309a0372de588d6b7f
kriss <kriss.clotilde@gmail.com>
2022-05-07 13:12:59 +0200

    nettoyage dependance ajout front

--- main2.js
+++ main2.js
@@ -2,7 +2,7 @@ var express = require('express');
 var bodyParser = require('body-parser');
 
 const cors=require('cors');
-
+;
 var routes = require('./public/app/controllers/routes');
 const todo = require('./public/app/models/todo');
 
@@ -12,21 +12,25 @@ var http=require('http').Server(app);
 app.use(bodyParser.urlencoded({extended:true}));
 app.use(bodyParser.json());
 
-app.get('/cookie', function(req, resp) {
 
-  resp.send({ status: 0, message: "ok" });
-})
 app.use(cors())
-app.use(express.static('public'))
-app.use('/app', express.static(__dirname + 'public/app'))
-app.use('/app/controllers', express.static(__dirname + 'public/app/controllers'))
-app.use('public/app/models', express.static(__dirname + 'public/app/models'))
+
+
+// Set Views
+
+
 
 
 routes.configure(app);
+//app.listen();
+
+
+
+
 
 
 console.log(3000);
+
 //pass a http.Server instance
 http.listen(3000);
 /*var server = app.listen(8000, function(){
